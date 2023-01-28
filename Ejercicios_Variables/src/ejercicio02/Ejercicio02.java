7c8a6219aa451680056aa2218dde61b33cc4652d
Dani <danielporras147@outlook.es>
2023-01-28 14:43:46 +0100

    Ejercicio 02

---
+++ Ejercicios_Variables/src/ejercicio02/Ejercicio02.java
@@ -0,0 +1,23 @@
+/**
+ * Ejercicio 2
+ * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+ * pantalla de tal forma que el resultado del programa sea el mismo que en el
+ * ejercicio 1 del capítulo 1.
+ */
+
+package ejercicio02;
+
+public class Ejercicio02 {
+	
+	public static void ejercicio02() {
+		String nombre = "Daniel Porras Pérez";
+		
+		System.out.println(nombre);
+	}
+
+	public static void main(String[] args) {
+		ejercicio02();
+
+	}
+
+}
