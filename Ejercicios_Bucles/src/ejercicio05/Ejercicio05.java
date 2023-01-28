096be8febdd57fc2ab3fd43ae98164209b2e7122
Dani <danielporras147@outlook.es>
2023-01-28 15:35:42 +0100

    Ejercicio 05

---
+++ Ejercicios_Bucles/src/ejercicio05/Ejercicio05.java
@@ -0,0 +1,26 @@
+/**
+ * Ejercicio 5
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+ * un bucle while.
+ */
+
+package ejercicio05;
+
+public class Ejercicio05 {
+	
+	public static void ejercicio05() {
+		int i = 320;
+		
+		while(i >= 160) {
+			System.out.println(i);
+			i -= 20;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		ejercicio05();
+
+	}
+
+}
