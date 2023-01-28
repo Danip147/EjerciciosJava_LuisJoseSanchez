e30d7e1aafb869760c471c6318e2c0f6588b494b
Dani <danielporras147@outlook.es>
2023-01-28 15:32:54 +0100

    Ejercicio 04

---
+++ Ejercicios_Bucles/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,22 @@
+/**
+ * Ejercicio 4
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+ * un bucle for.
+ */
+
+package ejercicio04;
+
+public class Ejercicio04 {
+	
+	public static void ejercicio04() {
+		for (int i = 320; i >= 160; i-=20) {
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		ejercicio04();
+
+	}
+
+}
