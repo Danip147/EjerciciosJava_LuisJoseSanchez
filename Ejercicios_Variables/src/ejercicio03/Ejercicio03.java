b1322d71bd7def5c23c670e46255dcd2e53ada1e
Dani <danielporras147@outlook.es>
2023-01-28 14:46:42 +0100

    Ejercicio 03

---
+++ Ejercicios_Variables/src/ejercicio03/Ejercicio03.java
@@ -0,0 +1,27 @@
+/**
+ * Ejercicio 3
+ * Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes.
+ * Muestra los valores de esas variables por pantalla de tal forma que el
+ * resultado del programa sea el mismo que en el ejercicio 2.
+ */
+
+package ejercicio03;
+
+public class Ejercicio03 {
+	
+	public static void ejercicio03() {
+		String nombre = "Daniel";
+		String direccion = "Alicante";
+		String telefono = "96478512345";
+		
+		System.out.println(nombre);
+		System.out.println(direccion);
+		System.out.println(telefono);
+	}
+
+	public static void main(String[] args) {
+		ejercicio03();
+
+	}
+
+}
