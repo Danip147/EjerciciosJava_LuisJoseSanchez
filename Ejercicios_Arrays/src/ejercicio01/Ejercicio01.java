43171998adfad4053f0a861db80a27c02c966e7a
Dani <danielporras147@outlook.es>
2023-01-30 19:32:52 +0100

    Ejercicio 01

---
+++ Ejercicios_Arrays/src/ejercicio01/Ejercicio01.java
@@ -0,0 +1,39 @@
+/**
+ * Ejercicio 1
+ * Define un array de 12 números enteros con nombre num y asigna los valores
+ * según la tabla que se muestra a continuación. Muestra el contenido de todos
+ * los elementos del array. ¿Qué sucede con los valores de los elementos que no
+ * han sido inicializados?
+ */
+
+package ejercicio01;
+
+public class Ejercicio01 {
+	
+	public static void ejercicio01() {
+		int [] num = new int[12];
+		
+		num[0] = 39; // Inicializar valores
+		num[1] = -2;
+		
+		 
+		num[4] = 0;
+		 
+		num[6] = 14;
+		 
+		num[8] = 5;
+		num[9] = 120;
+		
+		for(int numero : num) {
+			System.out.println(numero);
+		}
+		 
+		
+	}
+
+	public static void main(String[] args) {
+		ejercicio01();
+
+	}
+
+}
