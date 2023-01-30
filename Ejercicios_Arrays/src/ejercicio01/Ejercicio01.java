8c95dca0b56706ea8839214bf9f4a5dc61198227
Dani <danielporras147@outlook.es>
2023-01-30 19:38:24 +0100

    Ejercicio 01

--- Ejercicios_Arrays/src/ejercicio01/Ejercicio01.java
+++ Ejercicios_Arrays/src/ejercicio01/Ejercicio01.java
@@ -15,18 +15,16 @@ public class Ejercicio01 {
 		
 		num[0] = 39; // Inicializar valores
 		num[1] = -2;
-		
-		 
 		num[4] = 0;
-		 
 		num[6] = 14;
-		 
 		num[8] = 5;
 		num[9] = 120;
 		
 		for(int numero : num) {
 			System.out.println(numero);
 		}
+		
+		// los elementos que no han sido inicializados devuelve 0
 		 
 		
 	}
