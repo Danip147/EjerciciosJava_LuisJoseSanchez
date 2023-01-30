ee7b30614e91b545d9d14e41de1cb3353bca05a2
Dani <danielporras147@outlook.es>
2023-01-30 19:56:34 +0100

    Ejercicio 03

---
+++ Ejercicios_Arrays/src/ejercicio03/Ejercicio03.java
@@ -0,0 +1,50 @@
+/**
+ * Ejercicio 3
+ * Escribe un programa que lea 10 números por teclado y que luego los muestre
+ * en orden inverso, es decir, el primero que se introduce es el último en mostrarse
+ * y viceversa.
+ */
+
+package ejercicio03;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ejercicio03 {
+	
+	public static void ejercicio03() {
+		Scanner sc = new Scanner(System.in);
+		
+		Integer[] numeros = new Integer[5];
+		
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Introduce un número: ");
+			numeros[i] = sc.nextInt();
+		
+		}
+		
+		Arrays.sort(numeros); // Los ordena
+		
+		Collections.reverse(Arrays.asList(numeros));
+		
+		for(Integer numero : numeros) { // los muestra 
+			System.out.println(numero);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+	public static void main(String[] args) {
+		ejercicio03();
+
+	}
+
+}
